b8b36647034385ae4f0091fbcec13e2b9ef29635
Desiree <db.estudiocontablepy@gmail.com>
2024-11-06 21:09:12 -0300

    configuracion para gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),ghPages()],
+  base: "gestor-tareas",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
